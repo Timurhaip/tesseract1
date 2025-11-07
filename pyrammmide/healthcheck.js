e3ab5a44c92d14649e2f65c2ab36ce470d7799f0
Timurhaip <timur2010rumit0102@gmail.com>
2025-11-07 23:12:21 +0300

    Update healthcheck.js

--- pyrammmide/healthcheck.js
+++ pyrammmide/healthcheck.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
-app.get('/health', (req, res) => {
+app.get('/healthz', async (req, res) => {
  return res.sendStatus(200);
 });
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
 
